cdb8b2db128c7303b5c7aaed3aa1230ca5ec13ab
fcdsilva <me@fabiosilva.it>
2012-03-31 14:48:21 -0300

    Inicial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dimstore::Application.config.secret_token = '5411a98985e5a02a4eb7bb67f5b25e9329722d993325a644d6583ff5ddbef75723d187a81d4233a3ac6d90f146b8f831776e20c50eaeb68bdbc2942045d3abd3'
